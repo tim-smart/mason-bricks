8192c35616c89e7c957194c9fc2ced5d1c5c9ef2
Tim Smart <tim.smart@arisechurch.com>
2021-12-23 14:23:24 +1300

    Actually use HomeIntl in HomeScreen

--- bricks/timsmart_flutter_template/__brick__/lib/home/home_screen.dart
+++ bricks/timsmart_flutter_template/__brick__/lib/home/home_screen.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+import 'package:{{name}}/home/home_intl.dart';
 import 'package:{{name}}/shared/app_scaffold/app_scaffold.dart';
 
 part 'home_screen.g.dart';
 
 @swidget
-Widget homeScreen() => const AppSliverScaffold(
-      title: 'Home',
-      slivers: [
-        SliverFillRemaining(
-          child: Center(
-            child: Text('Home screen'),
-          ),
+Widget homeScreen(BuildContext context) {
+  final t = HomeIntl.of(context);
+
+  return AppSliverScaffold(
+    title: t.title,
+    slivers: [
+      SliverFillRemaining(
+        child: Center(
+          child: Text(t.body),
         ),
-      ],
-    );
+      ),
+    ],
+  );
+}
